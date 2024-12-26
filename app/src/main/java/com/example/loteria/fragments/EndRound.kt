f28231e1a1ffb0a57262eaa84f090236724b90b4
Mister-Mario <mariojr2003@gmail.com>
2024-12-26 21:16:31 +0100

    EndRound acabado

--- app/src/main/java/com/example/loteria/fragments/EndRound.kt
+++ app/src/main/java/com/example/loteria/fragments/EndRound.kt
@@ -5,15 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.loteria.R
+import com.example.loteria.databinding.FragmentEndRoundBinding
 
 class EndRound : Fragment() {
+
+    private lateinit var binding: FragmentEndRoundBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_end_round, container, false)
+        binding = FragmentEndRoundBinding.inflate(inflater, container, false)
+
+        binding.btnPlayAgain.setOnClickListener{
+            findNavController().navigate(EndRoundDirections.actionEndRoundToRound())
+        }
+
+        binding.btnFinish.setOnClickListener{
+            findNavController().navigate(EndRoundDirections.actionEndRoundToPrizes())
+        }
+
+        return binding.root
     }
 }
